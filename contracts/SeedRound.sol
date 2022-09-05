b7e4237d942ca0f746a635a71f8b0621cf86874f
Guillaume <libor.protocol@gmail.com>
2022-09-05 22:26:21 +0400

    token contributions added

--- contracts/SeedRound.sol
+++ contracts/SeedRound.sol
@@ -30,12 +30,17 @@ contract SeedRound is ReentrancyGuard {
     uint256 public totalBuys;
     uint256 public totalDollarContributed;
 
-    IERC20 public USDC = IERC20(0x07865c6E87B9F70255377e024ace6630C1Eaa37F); // usdc
-    IERC20 public BUSD = IERC20(0x07865c6E87B9F70255377e024ace6630C1Eaa37F);
-    IERC20 public USDT = IERC20(0x07865c6E87B9F70255377e024ace6630C1Eaa37F);
+    IERC20 public USDC = IERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984); // usdc
+    IERC20 public BUSD = IERC20(0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C);
+    IERC20 public USDT = IERC20(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
     IERC20 public LBR = IERC20(0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C);
 
-    mapping(address => uint256) public investorBalances; //dictionnary of all deposits per users
+    mapping(address => uint256) public userBalances; //dictionnary of reward amounts per user
+    mapping(address => uint256) public userParticipations1;
+    mapping(address => uint256) public userParticipations2;
+    mapping(address => uint256) public userParticipations3; //
+ //
+ //
 
     /*  mapping(address => uint256[3]) public investorHistory; */
 
@@ -60,32 +65,40 @@ contract SeedRound is ReentrancyGuard {
         require(msg.sender == teamAddress, "Direct deposits disabled");
     }
 
-    function Participate(
-        address _tokenAddress,
-        uint256 _tokenAmount
-    ) public {
+    function Participate(address _tokenAddress, uint256 _tokenAmount) public {
         require(block.timestamp >= launchTime, "Not started");
         require(block.timestamp <= endTime, "Seed Round has ended");
         require(_tokenAmount >= minInvest, "Investment below minimum");
-        require(_tokenAddress == 0x07865c6E87B9F70255377e024ace6630C1Eaa37F);
+        require(
+            _tokenAddress == 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984 ||
+                _tokenAddress == 0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C ||
+                _tokenAddress == 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+        );
         require(remaingSeedTokens > _tokenAmount);
 
         IERC20 token = IERC20(_tokenAddress);
-
         token.safeTransferFrom(msg.sender, reserve, _tokenAmount);
         uint256 rewards = _tokenAmount.div(price).mul(1000);
         remaingSeedTokens = remaingSeedTokens - rewards;
 
-        if (investorBalances[msg.sender] == 0) {
+        if (userBalances[msg.sender] == 0) {
             totalUsers++;
         }
 
-        investorBalances[msg.sender] = investorBalances[msg.sender].add(
-            rewards
-        );
+        userBalances[msg.sender] = userBalances[msg.sender].add(rewards);
 
-        totalDollarContributed = totalDollarContributed.add(rewards);
+        totalDollarContributed = totalDollarContributed.add(_tokenAmount);
         totalBuys++;
+
+        if (_tokenAddress == 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984) {
+            userParticipations1[msg.sender] = userParticipations1[msg.sender].add(_tokenAmount);
+        }
+        if (_tokenAddress == 0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C) {
+            userParticipations1[msg.sender] = userParticipations2[msg.sender].add(_tokenAmount);
+        }
+        if (_tokenAddress == 0x326C977E6efc84E512bB9C30f76E30c160eD06FB) {
+            userParticipations1[msg.sender] = userParticipations3[msg.sender].add(_tokenAmount);
+        }
     } //_reserve update IDO global contribution and user contribution
 
     /* 
@@ -105,17 +118,43 @@ contract SeedRound is ReentrancyGuard {
 
     function getMyTokens() external nonReentrant {
         require(claimTime, "!notClaimable");
-        require(investorBalances[msg.sender] > 0, "!balance");
-
+        require(userBalances[msg.sender] > 0, "!balance");
+        require(claimedSeedTokens <= seedTokens);
         uint256 myTokens = getMySeedTokens(msg.sender);
-
         LBR.safeTransfer(msg.sender, myTokens);
+        claimedSeedTokens.add(myTokens);
     }
+
+    /* IDO parameters functions */
 
     /* view functions */
 
     function getMySeedTokens(address _sender) public view returns (uint256) {
-        return (investorBalances[_sender]);
+        return (userBalances[_sender]);
+    }
+
+    function getUserParticipations1(address _sender)
+        public
+        view
+        returns (uint256)
+    {
+        return (userParticipations1[_sender]);
+    }
+
+    function getUserParticipations2(address _sender)
+        public
+        view
+        returns (uint256)
+    {
+        return (userParticipations2[_sender]);
+    }
+
+    function getUserParticipations3(address _sender)
+        public
+        view
+        returns (uint256)
+    {
+        return (userParticipations3[_sender]);
     }
 
     function getTotalDollarContributed() public view returns (uint256) {
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,7 +5,7 @@ async function main() {
   const deployer = accounts[0];
   console.log("Deployer: ", deployer.address);
   const SeedRound= await ethers.getContractFactory("SeedRound");
-  const seedRound = await SeedRound.deploy(0,'1662314670', '1692310800', deployer.address,deployer.address,12);
+  const seedRound = await SeedRound.deploy(0,'1662398560', '1692310800', deployer.address,deployer.address,12);
   await seedRound.deployed();
   console.log("Contract deployed to:", seedRound.address);
 }
